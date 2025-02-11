e0563bfade50a5a429bffa79eae787c569fb20ed
shalapy <salahshalapy999@gmail.com>
2025-02-11 23:59:59 +0200

    Added someRecursive problem description

---
+++ Algorithms/Recursion/someRecursive.js
@@ -0,0 +1,1 @@
+// Write a recursive function called someRecursive which accepts an array and a callback. The function returns true if a single value in the array returns true when passed to the callback. Otherwise it returns false.
