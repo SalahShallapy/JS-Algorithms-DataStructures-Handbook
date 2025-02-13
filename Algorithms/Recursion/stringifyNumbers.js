de78d5758ce01dd78ce351018f1033c4b8cae6f6
shalapy <salahshalapy999@gmail.com>
2025-02-13 23:59:59 +0200

    Added stringifyNumbers problem description

---
+++ Algorithms/Recursion/stringifyNumbers.js
@@ -0,0 +1,3 @@
+// Write a function called stringifyNumbers which takes in an object and finds all of the values which are numbers and converts them to strings. Recursion would be a great way to solve this!
+
+// The exercise intends for you to create a new object with the numbers converted to strings, and not modify the original. Keep the original object unchanged
