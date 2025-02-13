4acda8afba75fb9c3a3dc98c61dbcdc81546eb64
shalapy <salahshalapy999@gmail.com>
2025-02-13 23:59:59 +0200

    Implemnted recursive solution for nestedEvenSum problem

--- Algorithms/Recursion/nestedEvenSum.js
+++ Algorithms/Recursion/nestedEvenSum.js
@@ -1,1 +1,39 @@
 // Write a recursive function called nestedEvenSum. Return the sum of all even numbers in an object which may contain nested objects.
+
+function nestedEvenSum(obj) {
+  let sum = 0;
+
+  for (let key in obj) {
+    if (typeof obj[key] === "object") {
+      sum += nestedEvenSum(obj[key]); // Recursively call for nested objects
+    } else if (typeof obj[key] === "number" && obj[key] % 2 === 0) {
+      sum += obj[key]; // Add even numbers to sum
+    }
+  }
+
+  return sum;
+}
+
+// Example usage:
+var obj1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup",
+    },
+  },
+};
+
+var obj2 = {
+  a: 2,
+  b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+  c: { c: { c: 2 }, cc: "ball", ccc: 5 },
+  d: 1,
+  e: { e: { e: 2 }, ee: "car" },
+};
+
+console.log(nestedEvenSum(obj1)); // 6
+console.log(nestedEvenSum(obj2)); // 10
