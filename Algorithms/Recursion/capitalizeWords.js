a1bbb6c63d9bca74e4dfa886ea2432294792da51
shalapy <salahshalapy999@gmail.com>
2025-02-13 23:59:59 +0200

    Implemnted recursive solution for capitalizeWords problem

--- Algorithms/Recursion/capitalizeWords.js
+++ Algorithms/Recursion/capitalizeWords.js
@@ -1,1 +1,10 @@
 // Write a recursive function called capitalizeWords. Given an array of words, return a new array containing each word capitalized.
+
+function capitalizeWords(arr) {
+  if (arr.length === 0) return [];
+  let Caplazied = arr[0].toUpperCase();
+  return [Caplazied].concat(capitalizeWords(arr.slice(1)));
+}
+
+// let words = ['i', 'am', 'learning', 'recursion'];
+// capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
