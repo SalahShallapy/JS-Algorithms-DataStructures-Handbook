6cd59de02e83ba87ddb7f7d4769def1ec0d5d10e
shalapy <salahshalapy999@gmail.com>
2025-02-14 23:59:59 +0200

    Implemented a refactored binary search solution with O(log n) time complexity

--- Algorithms/Searching-Algorithms/binary_search.js
+++ Algorithms/Searching-Algorithms/binary_search.js
@@ -22,7 +22,19 @@ function binarySearch(arr, elem) {
 }
 
 // Refactored Version
+function binarySearch(arr, elem) {
+  var start = 0;
+  var end = arr.length - 1;
+  var middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[middle] === elem ? middle : -1;
+}
 
+binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103);
 // Examples:
 
 // binarySearch([1,2,3,4,5],2) // 1
