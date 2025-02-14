f4f475a5a351beb4e71136b6abb797c811098a94
shalapy <salahshalapy999@gmail.com>
2025-02-14 23:59:59 +0200

    Implemented a liner search solution with O(n) time complexity

--- Algorithms/Searching-Algorithms/linear_search.js
+++ Algorithms/Searching-Algorithms/linear_search.js
@@ -2,15 +2,6 @@
 
 // Don't use indexOf to implement this function!
 
-// Examples:
-
-// linearSearch([10, 15, 20, 25, 30], 15) // 1
-// linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4) // 5
-// linearSearch([100], 100) // 0
-// linearSearch([1,2,3,4,5], 6) // -1
-// linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 10) // -1
-// linearSearch([100], 200) // -1
-
 function linearSearch(arr, val) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === val) return i;
@@ -19,3 +10,12 @@ function linearSearch(arr, val) {
 }
 
 linearSearch([34, 51, 1, 2, 3, 45, 56, 687], 100);
+
+// Examples:
+
+// linearSearch([10, 15, 20, 25, 30], 15) // 1
+// linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4) // 5
+// linearSearch([100], 100) // 0
+// linearSearch([1,2,3,4,5], 6) // -1
+// linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 10) // -1
+// linearSearch([100], 200) // -1
