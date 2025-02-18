d2df80982351391210e686150d81e7fe8b5f3cd4
shalapy <salahshalapy999@gmail.com>
2025-02-18 23:59:59 +0200

    Implemented recursive merge sort function

--- Algorithms/Sorting-Algorithms/mergeSort.js
+++ Algorithms/Sorting-Algorithms/mergeSort.js
@@ -24,3 +24,14 @@ function merge(arr1, arr2) {
   }
   return results;
 }
+
+// Recrusive Merge Sort
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, sright);
+}
+
+mergeSort([10, 24, 76, 73]);
