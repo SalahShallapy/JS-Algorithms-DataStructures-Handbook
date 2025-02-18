4d228601f2f0594995fb24ff341d023691f13fef
shalapy <salahshalapy999@gmail.com>
2025-02-18 23:59:59 +0200

    Implemented merge helper function

--- Algorithms/Sorting-Algorithms/mergeSort.js
+++ Algorithms/Sorting-Algorithms/mergeSort.js
@@ -1,1 +1,26 @@
 // Implement the merge sort algorithm. Given an array, this algorithm will sort the values in the array. The functions take 2 parameters: an array and an optional comparator function.
+
+// Merge helper function
+function merge(arr1, arr2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
