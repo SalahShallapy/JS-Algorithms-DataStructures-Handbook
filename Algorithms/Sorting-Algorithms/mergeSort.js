2ab5a4c10b6896204cb9a31ada576576f715d3f2
shalapy <salahshalapy999@gmail.com>
2025-02-18 23:59:59 +0200

    Added mergre sort problem description

---
+++ Algorithms/Sorting-Algorithms/mergeSort.js
@@ -0,0 +1,1 @@
+// Implement the merge sort algorithm. Given an array, this algorithm will sort the values in the array. The functions take 2 parameters: an array and an optional comparator function.
