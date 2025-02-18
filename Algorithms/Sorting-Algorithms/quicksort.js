302abd852640af1d13325e5b720d30bc2bea99e9
shalapy <salahshalapy999@gmail.com>
2025-02-18 23:59:59 +0200

    Implemented recursive quick sort function

--- Algorithms/Sorting-Algorithms/quicksort.js
+++ Algorithms/Sorting-Algorithms/quicksort.js
@@ -30,3 +30,16 @@ function pivot(arr, start = 0, end = arr.length - 1) {
   swap(arr, start, swapIdx);
   return swapIdx;
 }
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right); //3
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+quickSort([100, -3, 2, 4, 6, 9, 1, 2, 5, 3, 23]);
