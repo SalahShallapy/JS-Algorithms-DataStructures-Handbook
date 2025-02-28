2c5da3df18d55a0de7c5100c9e284c2a2a8face0
shalapy <salahshalapy999@gmail.com>
2025-02-28 21:37:05 +0200

    Implemented the push method

--- DataStructures/Singly-Linked-Lists/SSL.js
+++ DataStructures/Singly-Linked-Lists/SSL.js
@@ -11,4 +11,18 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  // Push - Add node to the end
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
