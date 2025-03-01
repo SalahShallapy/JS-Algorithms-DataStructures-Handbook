2c64f34e0e4b1121696cd4a5e590bf1f49b0bed9
shalapy <salahshalapy999@gmail.com>
2025-03-01 23:59:59 +0200

    Implemented pop method

--- DataStructures/Stacks-And-Queues/stack.js
+++ DataStructures/Stacks-And-Queues/stack.js
@@ -25,4 +25,15 @@ class Stack {
     }
     return ++this.size;
   }
+
+  pop() {
+    if (!this.first) return null;
+    const temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.val;
+  }
 }
